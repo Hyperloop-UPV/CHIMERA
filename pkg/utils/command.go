5b7fadb11b0b93b00d560a55bac4631111f2a9fa
Javier Ribal del Rio <javierribal@gmail.com>
2026-02-07 18:57:31 +0100

    feat: run command

---
+++ pkg/utils/command.go
@@ -0,0 +1,18 @@
+package network
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+// Runs a command and prints its output. Returns an error if the command fails.
+func RunCommand(name string, args ...string) error {
+
+	cmd := exec.Command(name, args...)
+	output, err := cmd.CombinedOutput()
+
+	fmt.Printf("Running: %s %v\n", name, args)
+	fmt.Println(string(output))
+
+	return err
+}
