7b70e096a5c0beac1a12e4960d6e2a90b09a4b58
Javier Ribal del Rio <javierribal@gmail.com>
2026-04-07 21:52:33 +0200

    refactor(plate): refactor code using POO

--- pkg/plate/plate.go
+++ pkg/plate/plate.go
@@ -12,64 +12,66 @@ import (
 // NewPlateRuntime creates a new PlateRuntime for the given board and remote address. It resolves the local address based on the board's IP and creates a UDP connection to the backend. The local address is created as a dummy IP before, so it doesn't need to be actually assigned to an interface. The backend will receive the packets sent by the plate runtime and forward them to the decodification
 func NewPlateRuntime(board adj.Board, remoteAddrUDP *net.UDPAddr, portTCP uint16, period time.Duration) (*PlateRuntime, error) {
 
-	interfaceName, err := network.SetUpDummyInterface(board.Name, board.IP)
-	if err != nil {
-		return nil, fmt.Errorf("failed to set up dummy interface for board %s: %v", board.Name, err)
+	// Create plate runtime
+	plate := &PlateRuntime{
+		Board: board,
+	}
+
+	// Create dummy interface
+	if err := plate.createInterface(); err != nil {
+		return nil, fmt.Errorf("failed to create dummy interface for board %s: %v", board.Name, err)
 	}
 
 	// UDP
-	UDPconn, err := setupUDPConnection(board.IP, remoteAddrUDP)
-	if err != nil {
-		return nil, err
+	if err := plate.setupUDPConnection(remoteAddrUDP); err != nil {
+		plate.Delete() // Clean up the created interface
+		return nil, fmt.Errorf("failed to set up UDP connection for board %s: %v", board.Name, err)
 	}
 
-	TCPListener, err := setupTCPConnection(board.IP, portTCP)
-	if err != nil {
-		return nil, err
+	// TCP
+	if err := plate.setupTCPConnection(portTCP); err != nil {
+		plate.Delete() // Clean up the created interface
+		return nil, fmt.Errorf("failed to set up TCP connection for board %s: %v", board.Name, err)
 	}
 
-	plate := &PlateRuntime{
-		Board:              board,
-		UDPConn:            UDPconn,
-		TCPListener:        TCPListener,
-		BoardInterfaceName: interfaceName,
-	}
-
+	// Apply ADJ board configuration to the plate runtime
 	plate.applyADJBoardConfig(period) // Default period of 1 second for all packets, can be customized later
 
 	return plate, nil
 }
 
 // setupUDPConnection sets up a UDP connection to the backend
-func setupUDPConnection(localIP string, remoteAddr *net.UDPAddr) (*net.UDPConn, error) {
-	localAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:0", localIP))
+func (plate *PlateRuntime) setupUDPConnection(remoteAddr *net.UDPAddr) error {
+	localAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:0", plate.Board.IP))
 	if err != nil {
-		return nil, fmt.Errorf("error resolving local address: %v", err)
+		return fmt.Errorf("error resolving local address: %v", err)
 	}
 
 	conn, err := net.DialUDP("udp", localAddr, remoteAddr)
 	if err != nil {
-		return nil, fmt.Errorf("error dialing UDP connection: %v", err)
+		return fmt.Errorf("error dialing UDP connection: %v", err)
 	}
 
-	return conn, nil
+	plate.UDPConn = conn
+	return nil
 }
 
 // setupTCPConnection creates a TCP listener on the board IP and port
-func setupTCPConnection(boardIP string, portTCP uint16) (*net.TCPListener, error) {
+func (plate *PlateRuntime) setupTCPConnection(portTCP uint16) error {
 
 	// TCP
-	plateAddrTCP, err := net.ResolveTCPAddr("tcp", network.FormatIP(boardIP, int(portTCP)))
+	plateAddrTCP, err := net.ResolveTCPAddr("tcp", network.FormatIP(plate.Board.IP, int(portTCP)))
 	if err != nil {
-		return nil, fmt.Errorf("failed to resolve TCP listen address: %v", err)
+		return fmt.Errorf("failed to resolve TCP listen address: %v", err)
 	}
 
 	listener, err := net.ListenTCP("tcp", plateAddrTCP)
 	if err != nil {
-		return nil, fmt.Errorf("error creating TCP listener: %v", err)
+		return fmt.Errorf("error creating TCP listener: %v", err)
 	}
 
-	return listener, nil
+	plate.TCPListener = listener
+	return nil
 }
 
 // applyADJBoardConfig applies the configuration from the ADJ board to the plate runtime. It initializes the packets and measurements based on the ADJ board configuration.
@@ -107,6 +109,17 @@ func (plate *PlateRuntime) applyADJBoardConfig(period time.Duration) {
 			Measurements: measStates,
 		})
 	}
+}
+
+// createInterface creates a dummy interface
+func (plate *PlateRuntime) createInterface() error {
+	interfaceName, err := network.SetUpDummyInterface(plate.Board.Name, plate.Board.IP)
+	if err != nil {
+		return fmt.Errorf("failed to set up dummy interface for board %s: %v", plate.Board.Name, err)
+	}
+
+	plate.BoardInterfaceName = interfaceName
+	return nil
 }
 
 // Delete cleans up the plate runtime by closing connections and deleting the dummy interface.
